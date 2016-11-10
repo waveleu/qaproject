a520fe814d9ecaca4fa6801e693c2b774b01fe44
Liu <Bliu@verisilicon.com>
2016-11-10 10:32:21 +0800

    11-10 10:32

--- Application/Runtime/Cache/Admin/204bed0da3d426cd5d6cbc0735aec5ee.php
+++ Application/Runtime/Cache/Admin/204bed0da3d426cd5d6cbc0735aec5ee.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Amaze UI Admin table Examples</title>
-    <meta name="description" content="这是一个 table 页面">
+    <meta name="description" content="����һ�� table ҳ��">
     <meta name="keywords" content="table">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="renderer" content="webkit">
@@ -27,6 +27,20 @@
             -moz-appearance: none;
             -ms-appearance: none;
             appearance: none;
+        }
+        .am-modal-bd{
+            width:70%;
+            margin:0 auto;
+        }
+        .am-modal-bd input{
+            border:1px solid #9C9898;
+        }
+        .edit{
+            width:70%;
+            margin:0 auto;
+        }
+        .edit input{
+            border:1px solid #9C9898;
         }
     </style>
 </head>
@@ -74,10 +88,10 @@
                     <div class="am-modal am-modal-confirm" tabindex="<?php echo ($v[id]); ?>" >
                         <div class="am-modal-dialog">
                             <div class="am-modal-hd">Edit Board Type</div>
-                            <div class="am-modal-bd">
-                                <?php if(is_array($v)): $i = 0; $__LIST__ = $v;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vsub): $mod = ($i % 2 );++$i; if($key!=id): if($key==Type): echo ($key); ?>
+                            <div class="am-modal-bd edit">
+                                <?php if(is_array($v)): $i = 0; $__LIST__ = $v;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vsub): $mod = ($i % 2 );++$i; if($key!=id): if($key==Type): ?><label><?php echo ($key); ?></label>
                                             <div class="am-form-group-inline am-cf" >
-                                                <select data-am-selected   class="am-fr" placeholder="Please select..." onchange="edit_select(this)" id="<?php echo ($v[id]); ?>" name="<?php echo ($v[id]); ?>">
+                                                <select data-am-selected="{btnWidth: '80%', btnStyle: 'secondary'}"   class="am-fr" placeholder="Please select..." onchange="edit_select(this)" id="<?php echo ($v[id]); ?>" name="<?php echo ($v[id]); ?>">
                                                     <option value=""></option>
                                                     <option value="Chip" >Chip</option>
                                                     <option value="FPGA">FPGA</option>            
@@ -85,21 +99,24 @@
                                                 </select>
                                             </div>
                                     	<?php else: ?>
-	                                        <?php if($key==Customer): ?><span id="Customer_1"><?php echo ($key); ?></span>
+	                                        <?php if($key==Customer): ?><label id="Customer_1"><?php echo ($key); ?></label>
 	                                            <div class="am-form-group-inline am-cf" >
-	                                                <select data-am-selected   class="am-fr" placeholder="Please select..." id="Customer">
+	                                                <select data-am-selected="{btnWidth: '80%', btnStyle: 'secondary'}"   class="am-fr" placeholder="Please select..." id="Customer">
 	                                                    <option value=""></option>
 	                                                    <?php if(is_array($customer_list)): foreach($customer_list as $key=>$vc): if($vc==$vsub): ?><option value=<?php echo ($vc); ?> selected><?php echo ($vc); ?></option>
 	                                                        <?php else: ?>
 	                                                            <option value=<?php echo ($vc); ?>><?php echo ($vc); ?></option><?php endif; endforeach; endif; ?>
 	                                                </select>
 	                                            </div>
-	                                        <?php else: ?>
-	                                            <div id="<?php echo ($key); ?>"><?php echo ($key); ?><input type="text" class="am-modal-prompt-input" value="<?php echo ($vsub); ?>"></div><?php endif; endif; endif; endforeach; endif; else: echo "" ;endif; ?>
+    	                                        <?php else: ?>
+	                                            <div id="<?php echo ($key); ?>">
+                                                    <label><?php echo ($key); ?></label>
+                                                    <input type="text" class="am-modal-prompt-input" value="<?php echo ($vsub); ?>">
+                                                </div><?php endif; endif; endif; endforeach; endif; else: echo "" ;endif; ?>
                             </div>
                             <div class="am-modal-footer">
-                                <span class="am-modal-btn" data-am-modal-confirm style="width: 50%">OK</span>
-                                <span class="am-modal-btn" data-am-modal-cancel style="width: 50%">Cancel</span>
+                                <span class="am-modal-btn" data-am-modal-confirm style="width: 50%;border:1px solid #cccccc">OK</span>
+                                <span class="am-modal-btn" data-am-modal-cancel style="width: 50%;border:1px solid #cccccc">Cancel</span>
                             </div>
                         </div>
                     </div><?php endforeach; endif; ?>
@@ -113,53 +130,52 @@
 
 
 //add new board
-<div class="am-modal am-modal-confirm" tabindex="add_board_modal" id="add_board_modal" >
+<div class="am-modal am-modal-confirm" tabindex="add_board_modal" id="add_board_modal" style="top:-15%;">
     <div class="am-modal-dialog">
         <div class="am-modal-hd">New Board Type</div>
         <div class="am-modal-bd">
-	        <div id="Name_add">Name<input type="text" class="am-modal-prompt-input" id="Name"></div>
-	        <div id="Type_add">Type
-	                <div class="am-form-group-inline am-cf" >
-	                    <select data-am-selected    class="am-fr" placeholder="Please select..." onchange="new_select()" id="new_select" name="new_select">
-	                        <option value=""></option>
-	                        <option value="Chip">Chip</option>
-                            <option value="FPGA">FPGA</option>            
-                            <option value="CModel">CModel</option>
-	                    </select>
-	                </div>
+	        <div id="Name_add" class="Name_add">
+                <label>Name</label>
+                <input type="text" class="am-modal-prompt-input" id="Name">
+            </div>
+	        <div id="Type_add">
+                <label>Type</label>
+                <div class="am-form-group-inline am-cf">
+                    <select data-am-selected="{btnWidth: '80%', btnStyle: 'secondary'}"    class="am-fr" placeholder="Please select..." onchange="new_select()" id="new_select" name="new_select">
+                        <option value=""></option>
+                        <option value="Chip">Chip</option>
+                        <option value="FPGA">FPGA</option>            
+                        <option value="CModel">CModel</option>
+                    </select>
+                </div>
 	         </div>
-	        <div id="Customer_add">Customer
+	        <div id="Customer_add">
+                    <label>Customer</label>
 	                <div class="am-form-group-inline am-cf" >
-	                    <select data-am-selected    class="am-fr" placeholder="Please select...">
+	                    <select data-am-selected="{btnWidth: '80%', btnStyle: 'secondary'}" class="am-fr" placeholder="Please select...">
 	                        <option value=""></option>
 	                        <?php if(is_array($customer_list)): foreach($customer_list as $key=>$vc): ?><option value=<?php echo ($vc); ?>><?php echo ($vc); ?></option><?php endforeach; endif; ?>
 	                    </select>
 	                </div>
 	         </div>
-	        <div id="2D_Core_add">2D_Core<input type="text" class="am-modal-prompt-input" ></div>
-	        <div id="3D_Core_add">3D_Core<input type="text" class="am-modal-prompt-input" ></div>
-	        <div id="2D_VG_Core_add">2D_VG_Core<input type="text" class="am-modal-prompt-input" ></div>
-	        <div id="Bitfile_add">Bitfile<input type="text" class="am-modal-prompt-input" ></div>
-	        <div id="CModel_Location-P4_add">CModel_Location-P4<input type="text" class="am-modal-prompt-input" ></div>
-	        <div id="CModel_Location-Build_add">CModel_Location-Build<input type="text" class="am-modal-prompt-input" ></div>
+	        <div id="2D_Core_add"><label>2D_Core</label><input type="text" class="am-modal-prompt-input" ></div>
+	        <div id="3D_Core_add"><label>3D_Core</label><input type="text" class="am-modal-prompt-input" ></div>
+	        <div id="2D_VG_Core_add"><label>2D_VG_Core</label><input type="text" class="am-modal-prompt-input" ></div>
+	        <div id="Bitfile_add"><label>Bitfile</label><input type="text" class="am-modal-prompt-input" ></div>
+	        <div id="CModel_Location-P4_add"><label>CModel_Location-P4</label><input type="text" class="am-modal-prompt-input" ></div>
+	        <div id="CModel_Location-Build_add"><label>CModel_Location-Build</label><input type="text" class="am-modal-prompt-input" ></div>
         </div>
         <div class="am-modal-footer">
-            <span class="am-modal-btn" data-am-modal-confirm style="width: 50%">OK</span>
-            <span class="am-modal-btn" data-am-modal-cancel style="width: 50%">Cancel</span>
+            <span class="am-modal-btn" data-am-modal-confirm style="width: 50%;border:1px solid #cccccc">OK</span>
+            <span class="am-modal-btn" data-am-modal-cancel style="width: 50%;border:1px solid #cccccc"">Cancel</span>
         </div>
     </div>
 </div>
-
-
-
-
-
 <!--[if lt IE 9]>
 <script src="http://libs.baidu.com/jquery/1.11.1/jquery.min.js"></script>
 <script src="http://cdn.staticfile.org/modernizr/2.8.3/modernizr.js"></script>
 <script src="/qaweb/Public/assets/js/amazeui.ie8polyfill.min.js"></script>
 <![endif]-->
-
 <!--[if (gte IE 9)|!(IE)]><!-->
 <script src="/qaweb/Public/assets/js/jquery.min.js"></script>
 <!--<![endif]-->
@@ -184,7 +200,6 @@
                 e.close();
             }
         });
-
     }
     function del(obj) {
         var id=$(obj).attr('board_id');
@@ -193,7 +208,6 @@
                 window.location.reload();
             });
         }
-
     }
     function add() {
         $('#add_board_modal').modal({
@@ -215,36 +229,36 @@
     }
     
     $(document).ready(function(){
-    	$("#2D_Core,#3D_Core,#2D_VG_Core,#Bitfile,#CModel_Location-P4,#CModel_Location-Build").css("visibility","hidden");
-    	$("#2D_Core_add,#3D_Core_add,#2D_VG_Core_add,#Bitfile_add,#CModel_Location-P4_add,#CModel_Location-Build_add").css("visibility","hidden");	
+    	$("#2D_Core,#3D_Core,#2D_VG_Core,#Bitfile,#CModel_Location-P4,#CModel_Location-Build").css("display","none");
+    	$("#2D_Core_add,#3D_Core_add,#2D_VG_Core_add,#Bitfile_add,#CModel_Location-P4_add,#CModel_Location-Build_add").css("display","none");	
     });
     function edit_select(obj){
     	var id=$(obj).attr('id');
     	if($("#"+id).val()=='Chip'){
-    		$("#Customer_1,#Customer,#2D_Core,#3D_Core,#2D_VG_Core").css("visibility","visible");
-    		$("#Bitfile,#CModel_Location-P4,#CModel_Location-Build").css("visibility","hidden");
+    		$("#Customer_1,#2D_Core,#3D_Core,#2D_VG_Core").css("display","block");
+    		$("#Bitfile,#CModel_Location-P4,#CModel_Location-Build").css("display","none");
     		};
     	if($("#"+id).val()=='FPGA'){
-    		$("#Bitfile,#Customer").css("visibility","visible");
-    		$("#Customer_1,#Customer,#2D_Core,#3D_Core,#2D_VG_Core,#CModel_Location-P4,#CModel_Location-Build").css("visibility","hidden");
+    		$("#Customer_1,#Bitfile").css("display","block");
+    		$("#Customer,#2D_Core,#3D_Core,#2D_VG_Core,#CModel_Location-P4,#CModel_Location-Build").css("display","none");
     		};
     	if($("#"+id).val()=='CModel'){
-    		$("#CModel_Location-P4,#CModel_Location-Build,#3D_Core").css("visibility","visible");
-    		$("#Customer_1,#Customer,#2D_Core,#2D_VG_Core,#Bitfile").css("visibility","hidden");
+    		$("#Customer_1,#CModel_Location-P4,#CModel_Location-Build,#3D_Core").css("display","block");
+    		$("#Customer,#2D_Core,#2D_VG_Core,#Bitfile").css("display","none");
     		};
     	}
     function new_select(){
     	if($("#new_select").val()=='Chip'){
-    		$("#Customer_add,#2D_Core_add,#3D_Core_add,#2D_VG_Core_add").css("visibility","visible");
-    		$("#Bitfile_add,#CModel_Location-P4_add,#CModel_Location-Build_add").css("visibility","hidden");
+    		$("#Customer_add,#2D_Core_add,#3D_Core_add,#2D_VG_Core_add").css("display","block");
+    		$("#Bitfile_add,#CModel_Location-P4_add,#CModel_Location-Build_add").css("display","none");
     		};
     	if($("#new_select").val()=='FPGA'){
-    		$("#Bitfile_add").css("visibility","visible");
-    		$("#Customer_add,#2D_Core_add,#3D_Core_add,#2D_VG_Core_add,#CModel_Location-P4_add,#CModel_Location-Build_add").css("visibility","hidden");
+    		$("#Bitfile_add").css("display","block");
+    		$("#Customer_add,#2D_Core_add,#3D_Core_add,#2D_VG_Core_add,#CModel_Location-P4_add,#CModel_Location-Build_add").css("display","none");
     		};
     	if($("#new_select").val()=='CModel'){
-    		$("#CModel_Location-P4_add,#CModel_Location-Build_add,#3D_Core_add").css("visibility","visible");
-    		$("#Customer_add,#2D_Core_add,#2D_VG_Core_add,#Bitfile_add").css("visibility","hidden");
+    		$("#CModel_Location-P4_add,#CModel_Location-Build_add,#3D_Core_add").css("display","block");
+    		$("#Customer_add,#2D_Core_add,#2D_VG_Core_add,#Bitfile_add").css("display","none");
     		};
     	}
 </script>
